74cf62cf88c7143842e70575f3fd459275db3ebe
Mehul Patel <mehulp1123@gmail.com>
2021-01-08 18:41:28 -0500

    added PhotoUpload + tests

--- uploader/src/App.tsx
+++ uploader/src/App.tsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, {useState, FormEvent} from "react";
 import Container from "react-bootstrap/Container";
 import PhotoUpload from "./components/PhotoUpload";
 
 function App(): JSX.Element {
+	const [file, setFile] = useState<File | null>(null);
+
+	const handleSubmit = (e: FormEvent) => {
+		e.preventDefault;
+	};
+
+	const handleChange = (e: FormEvent) => {
+		const target: HTMLInputElement = e.target as HTMLInputElement;
+		setFile(target.files && target.files[0]);
+	};
+
 	return (
 		<Container>
-			<PhotoUpload />
+			<PhotoUpload handleSubmit={handleSubmit} handleChange={handleChange} />
 		</Container>
 	);
 }
--- uploader/src/components/PhotoUpload.tsx
+++ uploader/src/components/PhotoUpload.tsx
@@ -3,22 +3,28 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-// import '../styles.css'
 
-function PhotoUpload(): JSX.Element {
-	const handleSubmit = (e: FormEvent) => {
-		e.preventDefault;
-	};
+interface PhotoUploadProps {
+	handleSubmit<T = FormEvent>(T: FormEvent): void;
+	handleChange<T = FormEvent>(T: FormEvent): void;
+}
 
+function PhotoUpload(props: PhotoUploadProps): JSX.Element {
 	return (
 		<Row>
 			<Col className='form-container'>
-				<Form onSubmit={handleSubmit}>
+				<Form onSubmit={props.handleSubmit}>
 					<Form.Group className='form-label'>
-						<Form.File accept='image/png, image/jpg, image/jpeg, image/gif' label='Select Photo to Upload' id='form-file' />
+						<Form.File
+							accept='image/png, image/jpg, image/jpeg, image/gif'
+							label='Select Photo to Upload'
+							id='form-file'
+							onChange={props.handleChange}
+							data-testid='form-file'
+						/>
 					</Form.Group>
 					<Button type='submit' block>
-						Submit
+						Upload
 					</Button>
 				</Form>
 			</Col>
