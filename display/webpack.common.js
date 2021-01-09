df2b9876b9900e35d864c0a84b716853be7a0274
Mehul Patel <mehulp1123@gmail.com>
2021-01-08 19:29:34 -0500

    dev setup

---
+++ display/webpack.common.js
@@ -0,0 +1,64 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+module.exports = {
+	entry: "./src/index.tsx",
+	target: "web",
+	plugins: [
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			template: path.join(__dirname, "src", "index.html")
+		}),
+		new MiniCssExtractPlugin({
+			filename: "[name].css",
+			chunkFilename: "[id].css"
+		})
+	],
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/i,
+				use: "ts-loader",
+				exclude: /node_modules/
+			},
+			{
+				enforce: "pre",
+				test: /\.js$/,
+				use: "source-map-loader"
+			},
+			{
+				test: /\.css$/i,
+				use: [
+					MiniCssExtractPlugin.loader,
+					// 'style-loader',
+					"css-loader"
+				]
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/i,
+				use: "file-loader"
+			}
+		]
+	},
+	resolve: {
+		extensions: [".tsx", ".ts", ".jsx", ".js", "css"]
+	},
+	optimization: {
+		minimizer: [new CssMinimizerPlugin()],
+		moduleIds: "deterministic",
+		runtimeChunk: "single",
+		splitChunks: {
+			cacheGroups: {
+				vendor: {
+					test: /[\\/]node_modules[\\/]/,
+					name: "vendors",
+					chunks: "all"
+				}
+			}
+		}
+	}
+};
