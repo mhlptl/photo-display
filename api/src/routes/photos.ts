64abc448266b9ca8cf1002856b90b33dea851eef
Mehul Patel <mehulp1123@gmail.com>
2021-01-11 21:36:12 -0500

    added tests

--- api/src/routes/photos.ts
+++ api/src/routes/photos.ts
@@ -2,14 +2,20 @@ import express from 'express';
 import multer from 'multer';
 import {storage, fileFilter, addFile, getRandomFile} from '../utils';
 import * as path from 'path'
+import e from 'express';
 
 const router : express.Router = express.Router();
 
 const upload: multer.Multer = multer({storage: storage, fileFilter: fileFilter});
 
 router.post('/photos/upload', upload.single('image'), async(req: express.Request, res: express.Response) => {
-	await addFile(req.file.filename);
-	res.status(201).json({message: 'Photo Uploaded'});
+	if(req.file === undefined) {
+		res.status(404).json({message: 'error, please try again'});
+	}
+	else {
+		await addFile(req.file.filename);
+		res.status(201).json({message: 'Photo Uploaded'});
+	}
 });
 
 router.post('/photos/random', async(req: express.Request, res: express.Response) => {
--- api/src/server.ts
+++ api/src/server.ts
@@ -1,16 +1,6 @@
-import express from "express";
-
-const app: express.Application = express();
+import {app} from './index';
 
 const PORT: number = parseInt(process.env.PORT || '5000');
-
-import indexRouter from "./routes/index";
-import photosRouter from './routes/photos';
-
-const baseUrl = '/api/v1'
-
-app.use(baseUrl, indexRouter);
-app.use(baseUrl, photosRouter);
 
 app.listen(PORT, () => {
 	console.log(`Server listening at PORT=${PORT}`);
