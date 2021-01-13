a01b33015fb908c3ef50c0416883fac595d1cf14
Mehul Patel <mehulp1123@gmail.com>
2021-01-13 12:36:30 -0500

    added cors functionality

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
+import cors from "cors";
 
 const app: express.Application = express();
+app.use(cors());
 
 import indexRouter from "./routes/index";
 import photosRouter from './routes/photos';
